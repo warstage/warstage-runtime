c7a561c5e6e80040fecf6acf14e897419e58277f
Felix Ungman <felix@ungman.com>
2020-09-30 22:19:10 +0200

    EmbeddedConnection

--- src/embedded-connection.ts
+++ src/embedded-connection.ts
@@ -37,33 +37,7 @@ export class EmbeddedConnection implements RuntimeConnection {
         if (!this.messageListener) {
             this.compressor = new Compressor();
             this.decompressor = new Decompressor();
-            this.messageListener = event => {
-                if (event.source === window.parent) {
-                    if (event.data.packet) {
-                        if (this.onPacketCallback) {
-                            const arrayBuffer = event.data.packet;
-                            const buffer = Buffer.from(arrayBuffer, 0, arrayBuffer.byteLength)
-                            const packet = this.decompressor.decode(buffer) as Payload;
-                            this.onPacketCallback(packet);
-                        } else {
-                            console.error('EmbeddedConnection: missing onPacket callback');
-                        }
-                    } else if (event.data.open) {
-                        this.stopSendOpen();
-                        if (this.onOpenCallback) {
-                            this.onOpenCallback();
-                        }
-                    } else if (event.data.close) {
-                        this.stopSendOpen();
-                        window.removeEventListener('message', this.messageListener);
-                        this.messageListener = null;
-                        if (this.onCloseCallback) {
-                            this.onCloseCallback();
-                        }
-                    }
-                }
-            };
-            window.addEventListener('message', this.messageListener);
+            this.addMessageListener();
             this.startSendOpen();
         } else {
             console.warn('EmbeddedConnection.open(): session is already opened');
@@ -73,21 +47,61 @@ export class EmbeddedConnection implements RuntimeConnection {
     close() {
         this.stopSendOpen();
         if (this.messageListener) {
-            window.parent.postMessage({ close: true }, '*');
+            this.sendMessage({ close: true });
         } else {
             console.warn('EmbeddedConnection.close(): session is not open');
         }
     }
 
     sendPacket(payload: Payload) {
-        window.parent.postMessage({ packet: this.compressor.encode(payload) }, '*');
+        this.sendMessage({ packet: this.compressor.encode(payload) });
+    }
+
+    private sendMessage(message: any) {
+        console.log('embedded send ' + JSON.stringify(message));
+        window.parent.postMessage(message, '*');
+    }
+
+    private addMessageListener() {
+        this.messageListener = event => {
+            if (event.source === window.parent) {
+                console.log('embedded onmessage ' + JSON.stringify(event.data));
+                if (event.data.packet) {
+                    if (this.onPacketCallback) {
+                        const arrayBuffer = event.data.packet;
+                        const buffer = Buffer.from(arrayBuffer, 0, arrayBuffer.byteLength)
+                        const packet = this.decompressor.decode(buffer) as Payload;
+                        this.onPacketCallback(packet);
+                    } else {
+                        console.error('EmbeddedConnection: missing onPacket callback');
+                    }
+                } else if (event.data.open) {
+                    this.stopSendOpen();
+                    if (this.onOpenCallback) {
+                        this.onOpenCallback();
+                    }
+                } else if (event.data.close) {
+                    this.stopSendOpen();
+                    this.removeMessageListener();
+                    if (this.onCloseCallback) {
+                        this.onCloseCallback();
+                    }
+                }
+            }
+        };
+        window.addEventListener('message', this.messageListener);
+    }
+
+    private removeMessageListener() {
+        window.removeEventListener('message', this.messageListener);
+        this.messageListener = null;
     }
 
     private startSendOpen() {
-        window.parent.postMessage({open: true}, '*');
+        this.sendMessage({ open: true });
         this.openerInterval = setInterval(() => {
             if (this.openerInterval) {
-                window.parent.postMessage({open: true}, '*');
+                this.sendMessage({ open: true });
             }
         }, 200);
     }
