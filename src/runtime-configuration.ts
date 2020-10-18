bd1140a4f9868cf6222053cf556c11a45c18d002
Felix Ungman <felix@ungman.com>
2020-10-18 22:40:58 +0200

    Update auto-redirect url

--- src/runtime-configuration.ts
+++ src/runtime-configuration.ts
@@ -16,7 +16,7 @@ export class RuntimeConfiguration {
 
     static autoRedirect() {
         if (window === window.parent) {
-            window.location.href = 'https://warstage.net/index.html?module=' + encodeURI(window.location.toString());
+            window.location.href = 'https://warstage.net/index.html#' + window.location.toString();
         }
     }
 
